dd2d8ddf7421977526f2d9c25018dc75ee70caed
WakhidHasim <wakhidhasim2000@gmail.com>
2025-06-25 23:35:48 +0700

    refactor login view and remove unused seeder

--- resources/views/pages/auth/login.blade.php
+++ resources/views/pages/auth/login.blade.php
@@ -1,27 +1,28 @@
 @extends('layouts.auth.app')
+
 @section('content')
-      <div class="container">
+    <div class="container">
         <div class="row align-items-center justify-content-center">
-          <div class="col-md-7">
-            <div class="mb-4">
-              <h3>Login</h3>
+            <div class="col-md-7">
+                <div class="mb-4">
+                    <h3>Login</h3>
+                </div>
+                <form action="{{ route('login') }}" method="post">
+                    @csrf
+                    <div class="form-group first">
+                        <label for="email">Email</label>
+                        <input type="email" class="form-control" id="email">
+
+                    </div>
+                    <div class="form-group last mb-3">
+                        <label for="password">Password</label>
+                        <input type="password" class="form-control" id="password">
+                    </div>
+                    <div class="d-flex mb-5 align-items-center">
+                    </div>
+                    <button class="btn btn-block btn-success" type="submit">Masuk</button>
+                </form>
             </div>
-            <form action="#" method="post">
-              <div class="form-group first">
-                <label for="username">Username</label>
-                <input type="text" class="form-control" id="username">
-
-              </div>
-              <div class="form-group last mb-3">
-                <label for="password">Password</label>
-                <input type="password" class="form-control" id="password">
-              </div>
-              <div class="d-flex mb-5 align-items-center">
-              </div>
-              <input type="submit" value="Log In" class="btn btn-block btn-success">
-            </form>
-          </div>
         </div>
-      </div>
-
+    </div>
 @endsection
